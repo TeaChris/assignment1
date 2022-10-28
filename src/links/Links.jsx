42a0acd8f683dc9ca68c3a3e9633ab7c25cb4ba3
bermuda <teechris110@gmail.com>
2022-10-28 11:46:21 +0100

    add social icons

--- src/links/Links.jsx
+++ src/links/Links.jsx
@@ -1,3 +1,5 @@
+import IMG1 from '../assets/Icon.png'
+import IMG2 from '../assets/Icon2.png'
 import './links.css'
 
 const Links = () => {
@@ -9,6 +11,10 @@ const Links = () => {
         <button className='link_btn'><a href="" id='' target='_blank'>Python Books</a></button>
         <button className='link_btn'><a href="" id='' target='_blank'>Background check for coders</a></button>
         <button className='link_btn'><a href="" id='' target='_blank'>Design Books</a></button>
+        <div className="social_icon">
+          <a href="#"><img src={IMG2} alt="Github Logo" className='slack'/></a>
+          <a href="#"><img src={IMG1} alt="Github Logo" /></a>
+        </div>
     </section>
   )
 }
