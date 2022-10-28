63537a9fddd31d921ebdd48549d34c66364f7e61
bermuda <teechris110@gmail.com>
2022-10-28 10:14:57 +0100

    added images, fixed bugs

--- src/links/Links.jsx
+++ src/links/Links.jsx
@@ -3,12 +3,12 @@ import './links.css'
 const Links = () => {
   return (
      <section className='links'>
-        <button><a href="" id='' target='_blank' className='twitter'>Twitter Link</a></button>
-        <button><a href="" id='' target='_blank'>Zuri Team</a></button>
-        <button><a href="" id='' target='_blank'>Zuri Books</a></button>
-        <button><a href="" id='' target='_blank'>Python Books</a></button>
-        <button><a href="" id='' target='_blank'>Background check for coders</a></button>
-        <button><a href="" id='' target='_blank'>Design Books</a></button>
+        <button className='link_btn'><a href="" id='' target='_blank' className='twitter'>Twitter Link</a></button>
+        <button className='link_btn'><a href="" id='' target='_blank'>Zuri Team</a></button>
+        <button className='link_btn'><a href="" id='' target='_blank'>Zuri Books</a></button>
+        <button className='link_btn'><a href="" id='' target='_blank'>Python Books</a></button>
+        <button className='link_btn'><a href="" id='' target='_blank'>Background check for coders</a></button>
+        <button className='link_btn'><a href="" id='' target='_blank'>Design Books</a></button>
     </section>
   )
 }
--- src/profile/Profile.jsx
+++ src/profile/Profile.jsx
@@ -1,17 +1,20 @@
-// import {BsArrow90DegRight} from 'react-icons/bs'
-import ReplyIcon from '@mui/icons-material/Reply';
 import IMG1 from '../assets/profile-img.png'
+import IMG2 from '../assets/share.png'
+import IMG3 from '../assets/share-moile.png'
 import './profile.css'
 
 const Profile = () => {
   return (
     <header>
-      <div className="header_icon">
-        <ReplyIcon/>
-        {/* <BsArrow90DegRight/> */}
-      </div>
+      {/* <div className="header_image">
+        <a href="#">
+          <img src={IMG2} alt="Share button" />
+        </a>
+      </div> */}
       <div className="profile">
-        <img src={IMG1} alt="" />
+        <img src={IMG1} alt="Profile Photo" className='profile_photo'/>
+        <a href="#"><img src={IMG2} alt="Share button" className='share'/></a>
+        <a href="#"><img src={IMG3} alt="Share button" className='share_mobile'/></a>
       </div>
       <h2>Oyabkin Temitope</h2>
     </header>
